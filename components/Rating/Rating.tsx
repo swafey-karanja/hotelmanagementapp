5af412ff1d74363fb66ccb77bb0a3ccef54640f9
swafey karanja <kswafey@gmail.com>
2024-08-21 12:06:38 +0300

    lost one

---
+++ components/Rating/Rating.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react"
+import { FaStar, FaStarHalf } from "react-icons/fa";
+
+const Rating: FC<{rating: number}> = ({ rating }) => {
+
+    const fullStars = Math.floor(rating);
+    const decimalPart = rating - fullStars;
+
+    const fullStarElements = Array(fullStars).fill(<FaStar />);
+
+    let halfStarElement = null;
+
+    if (decimalPart > 0) {
+        halfStarElement = <FaStarHalf />;
+    }
+
+  return (
+    <>{fullStarElements} {halfStarElement}</>
+  )
+}
+
+export default Rating
